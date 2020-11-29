660301685285c4d67727895ac2e73c057276d354
Otsuki Hitoshi <hitochan777@gmail.com>
2020-11-30 00:58:59 +0900

    Add stepper

--- client/components/SearchForm.tsx
+++ client/components/SearchForm.tsx
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
+import { CSSTransition } from "react-transition-group";
 
 import { ErrorMessage } from "./ErrorMessage";
 
 interface Props {
   onSubmit: (origin: string, destionations: string[]) => Promise<void>;
   isLoading: boolean;
+  step: number;
 }
 
+const useStepper = (initialStep = 1) => {
+  const [step, setStep] = useState(initialStep);
+  return {
+    step,
+    next: () => {
+      if (step === 0) {
+        return;
+      }
+      setStep((prevStep) => prevStep + 1);
+    },
+    back: () => {
+      setStep((prevStep) => prevStep - 1);
+    },
+  };
+};
+
 export const SearchForm: React.FC<Props> = ({ onSubmit, isLoading }) => {
+  const { step, next, back } = useStepper(1);
   return (
     <Formik
       initialValues={{ origin: "", destinations: "" }}
@@ -27,35 +46,61 @@ export const SearchForm: React.FC<Props> = ({ onSubmit, isLoading }) => {
         }
       }}
     >
-      <Form>
-        <div className="form-control">
-          <div className="flex items-center flex-end">
-            <ErrorMessage name="origin" />
-          </div>
-          <Field
-            className="width-100 round-border"
-            name="origin"
-            type="text"
-            placeholder="Departing from"
-          />
-        </div>
-
-        <div className="form-control">
-          <div className="flex items-center flex-end">
-            <ErrorMessage name="destinations" />
-          </div>
-          <Field
-            className="width-100 round-border"
-            name="destinations"
-            as="textarea"
-            rows={10}
-            placeholder={"Destination A\nDestination B\nDestination C"}
-          />
-        </div>
-        <div className="flex justify-center">
-          <button type="submit" disabled={isLoading}>{isLoading ? "Searching..." : "Search" }</button>
-        </div>
-      </Form>
+      {({ validateField, errors }) => (
+        <Form>
+          <>
+            {step === 1 && (
+              <div className="form-control">
+                <Field
+                  className="width-100 round-border"
+                  name="origin"
+                  type="text"
+                  placeholder="Departing from"
+                />
+                <div className="flex items-center justify-center">
+                  <ErrorMessage name="origin" />
+                </div>
+                <div className="flex justify-center">
+                  <button
+                    onClick={() => {
+                      validateField("origin");
+                      if (!errors.origin) {
+                        next();
+                      }
+                    }}
+                  >
+                    Next
+                  </button>
+                </div>
+              </div>
+            )}
+            {step === 2 && (
+              <>
+                <div className="form-control">
+                  <Field
+                    className="width-100 round-border"
+                    name="destinations"
+                    as="textarea"
+                    rows={10}
+                    placeholder={"Destination A\nDestination B\nDestination C"}
+                  />
+                  <div className="flex items-center justify-center">
+                    <ErrorMessage name="destinations" />
+                  </div>
+                </div>
+                <div className="flex justify-center">
+                  <button onClick={back} disabled={isLoading}>
+                    Back
+                  </button>
+                  <button type="submit" disabled={isLoading}>
+                    {isLoading ? "Searching..." : "Search"}
+                  </button>
+                </div>
+              </>
+            )}
+          </>
+        </Form>
+      )}
     </Formik>
   );
 };
