57c95fb8bb48109b1da6b3b44b133af0879393f6
Otsuki Hitoshi <hitochan777@gmail.com>
2020-11-30 22:55:20 +0900

    Fix styling

--- client/components/SearchForm.tsx
+++ client/components/SearchForm.tsx
@@ -8,7 +8,6 @@ import { ErrorMessage } from "./ErrorMessage";
 interface Props {
   onSubmit: (origin: string, destionations: string[]) => Promise<void>;
   isLoading: boolean;
-  step: number;
 }
 
 const useStepper = (initialStep = 1) => {
@@ -51,17 +50,18 @@ export const SearchForm: React.FC<Props> = ({ onSubmit, isLoading }) => {
           <SwitchTransition>
             <CSSTransition key={step} classNames="fade" timeout={200}>
               <>
-              {step === 1 && (
-                <>
-                  <div className="form-control">
-                    <Field
-                      className="width-100 round-border"
-                      name="origin"
-                      type="text"
-                      placeholder="Departing from"
-                    />
-                    <div className="flex items-center justify-center">
-                      <ErrorMessage name="origin" />
+                {step === 1 && (
+                  <>
+                    <div className="form-control">
+                      <Field
+                        className="width-100 round-border"
+                        name="origin"
+                        type="text"
+                        placeholder="Departing from"
+                      />
+                      <div className="flex items-center justify-center">
+                        <ErrorMessage name="origin" />
+                      </div>
                     </div>
                     <div className="flex justify-center">
                       <button
@@ -75,35 +75,34 @@ export const SearchForm: React.FC<Props> = ({ onSubmit, isLoading }) => {
                         Next
                       </button>
                     </div>
-                  </div>
-                </>
-              )}
-              {step === 2 && (
-                <>
-                  <div className="form-control">
-                    <Field
-                      className="width-100 round-border"
-                      name="destinations"
-                      as="textarea"
-                      rows={10}
-                      placeholder={
-                        "Destination A\nDestination B\nDestination C"
-                      }
-                    />
-                    <div className="flex items-center justify-center">
-                      <ErrorMessage name="destinations" />
+                  </>
+                )}
+                {step === 2 && (
+                  <>
+                    <div className="form-control">
+                      <Field
+                        className="width-100 round-border"
+                        name="destinations"
+                        as="textarea"
+                        rows={10}
+                        placeholder={
+                          "Destination A\nDestination B\nDestination C"
+                        }
+                      />
+                      <div className="flex items-center justify-center">
+                        <ErrorMessage name="destinations" />
+                      </div>
                     </div>
-                  </div>
-                  <div className="flex justify-center">
-                    <button onClick={back} disabled={isLoading}>
-                      Back
-                    </button>
-                    <button type="submit" disabled={isLoading}>
-                      {isLoading ? "Searching..." : "Search"}
-                    </button>
-                  </div>
-                </>
-              )}
+                    <div className="flex justify-center">
+                      <button onClick={back} disabled={isLoading}>
+                        Back
+                      </button>
+                      <button type="submit" disabled={isLoading}>
+                        {isLoading ? "Searching..." : "Search"}
+                      </button>
+                    </div>
+                  </>
+                )}
               </>
             </CSSTransition>
           </SwitchTransition>
